0ae1a6c146193365538626f3d6b19459157cf8e6
Jhonny0102 <fjhon868@gmail.com>
2024-04-23 12:45:16 +0200

    martes mañana

--- ApiCentroMedicoClient/Services/ServiceApiCentroMedicoClient.cs
+++ ApiCentroMedicoClient/Services/ServiceApiCentroMedicoClient.cs
@@ -1,9 +1,15 @@
 ﻿using ApiCentroMedicoClient.Models;
+using Azure;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using NuGet.Common;
+using System.Diagnostics.Contracts;
 using System.Net.Http.Headers;
 using System.Numerics;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ApiCentroMedicoClient.Services
 {
@@ -94,6 +100,33 @@ namespace ApiCentroMedicoClient.Services
             }
         }
 
+        //Sobrecarga de otro metodo.
+        //1. Metodo para actualizar(PUT) donde se pasa request , generico(T).
+        private async Task CallPutApiAsync<T>(string request, T objeto)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(this.UrlApiCentro);
+                client.DefaultRequestHeaders.Clear();
+                string json = JsonConvert.SerializeObject(objeto);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PutAsync(request, content);
+            }
+        }
+        //2. Metodo para actualizar(PUT) donde se pasa request , generico(T) , token.
+        private async Task CallPutApiAsync<T>(string request, T objeto, string token)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(this.UrlApiCentro);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Add("Authorization", "bearer " + token);
+                string json = JsonConvert.SerializeObject(objeto);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PutAsync(request, content);
+            }
+        }
+
         // ==================== Metodos Login ==================== //
 
         //Metodo para obtener informacion basica de USUARIO mediante el correo y la contra(NO TOKEN).
@@ -118,26 +151,17 @@ namespace ApiCentroMedicoClient.Services
         //Metodo para editar el/la recepcionista.
         public async Task PutRecepcionista(int id, string nombre , string apellido , string correo , string contra , int estado , int tipo)
         {
-            using (HttpClient client = new HttpClient())
-            {
-                string request = "api/usuarios";
-                string token = this.httpContextAccessor.HttpContext.User.FindFirst(z => z.Type == "TOKEN").Value;
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer ", token);
-                client.BaseAddress = new Uri(this.UrlApiCentro);
-                client.DefaultRequestHeaders.Clear();
-                client.DefaultRequestHeaders.Accept.Add(this.Header);
-                Usuario user = new Usuario();
-                user.Id = id;
-                user.Nombre = nombre;
-                user.Apellido = apellido;
-                user.Correo = correo;
-                user.Contra = contra;
-                user.Id_EstadoUsuario = estado;
-                user.Id_TipoUsuario = tipo;
-                string json = JsonConvert.SerializeObject(user);
-                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
-                HttpResponseMessage reponse = await client.PutAsync(request, content);
-            }
+            string request = "api/usuarios";
+            string token = this.httpContextAccessor.HttpContext.User.FindFirst(z => z.Type == "TOKEN").Value;
+            Usuario user = new Usuario();
+            user.Id = id;
+            user.Nombre = nombre;
+            user.Apellido = apellido;
+            user.Correo = correo;
+            user.Contra = contra;
+            user.Id_EstadoUsuario = estado;
+            user.Id_TipoUsuario = tipo;
+            this.CallPutApiAsync(request, user, token);
         }
 
         //Metodo para encontrar un paciente por su nombre , apellido y correo.
